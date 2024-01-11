d6058abee32464a8155caa9f060690b51f91d11c
ddl-peter-lu <127427678+ddl-peter-lu@users.noreply.github.com>
2024-01-11 15:18:25 -0800

    Update promptsConfig.ts

--- src/config/promptsConfig.ts
+++ src/config/promptsConfig.ts
@@ -4,7 +4,10 @@ enum Prompt {
 
 const promptsConfig: { [key in Prompt]: string } = {
   [Prompt.SYSTEM_PROMPT]:
+    'You now assume the role of a technical writer editor for the public online-documentation site for an MLOps platform. Based on the patch provide a list of suggestions how to improve the text with examples looking at spelling, grammar, and clarity.\nStart every suggestion with path to the file. Path to the file should start with @@ and end with @@',    
+    /**
     'You now assume the role of a code reviewer. Based on the patch provide a list of suggestions how to improve the code with examples according to coding standards and best practices.\nStart every suggestion with path to the file. Path to the file should start with @@ and end with @@',
+    **/
 };
 
 export default promptsConfig;
